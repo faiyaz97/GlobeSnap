e52a9f9abcf6bb24c5c39b865c0313c05b2a7f53
faiyaz97 <faiyaz.f.uddin@gmail.com>
2023-04-04 15:40:33 +0100

    changed user widget on left design

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -3,6 +3,8 @@ import {
   EditOutlined,
   LocationOnOutlined,
   WorkOutlineOutlined,
+  Instagram,
+  Facebook,
 } from "@mui/icons-material";
 import { Box, Typography, Divider, useTheme } from "@mui/material";
 import UserImage from "components/UserImage";
@@ -38,111 +40,108 @@ const UserWidget = ({ userId, picturePath }) => {
     return null;
   }
 
-  const {
-    firstName,
-    lastName,
-    location,
-    viewedProfile,
-    impressions,
-    friends,
-  } = user;
+  const { firstName, lastName, location, viewedProfile, impressions, friends } =
+    user;
 
   return (
     <WidgetWrapper>
-      {/* FIRST ROW */}
-      <FlexBetween
-        gap="0.5rem"
-        pb="1.1rem"
-        onClick={() => navigate(`/profile/${userId}`)}
-      >
-        <FlexBetween gap="1rem">
-          <UserImage image={picturePath} />
+      <Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="h6" sx={{ marginRight: "0.5rem" }}>
+            {location.id}
+          </Typography>
+          <img
+            src={`https://flagcdn.com/w20/${location.code.toLowerCase()}.png`}
+            srcSet={`https://flagcdn.com/w40/${location.code.toLowerCase()}.png 2x`}
+            alt={location.name}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column", // Add this line
+            alignItems: "center",
+            justifyContent: "center",
+            mt: "1rem",
+          }}
+        >
+          <Box>
+            <UserImage image={picturePath} size="100px" />
+          </Box>
           <Box>
             <Typography
               variant="h4"
-              color={dark}
               fontWeight="500"
               sx={{
                 "&:hover": {
-                  color: palette.primary.light,
                   cursor: "pointer",
                 },
               }}
             >
               {firstName} {lastName}
             </Typography>
-            <Typography color={medium}>{friends.length} friends</Typography>
+          </Box>
+          <Box sx={{ mt: "0.75rem" }}>
+            <Typography variant="body1" color={main}>
+              Questa e' la vita nostra. Ciao!
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Divider sx={{ m: "1.5rem 0" }} />
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Typography variant="h6">Posts</Typography>
+          <Typography variant="body1" color={main} sx={{ textAlign: "center" }}>
+            22
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h6">Followers</Typography>
+          <Typography variant="body1" color={main} sx={{ textAlign: "center" }}>
+            777
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6">Following</Typography>
+          <Typography variant="body1" color={main} sx={{ textAlign: "center" }}>
+            2
+          </Typography>
+        </Box>
+      </Box>
+
+      <Divider sx={{ m: "1.5rem 0" }} />
+
+      <FlexBetween gap="1rem" mb="0.5rem">
+        <FlexBetween gap="1rem">
+          <Instagram fontSize="large" />
+          <Box>
+            <Typography color={main} fontWeight="500">
+              Instagram.user
+            </Typography>
           </Box>
         </FlexBetween>
-        <ManageAccountsOutlined />
       </FlexBetween>
 
-      <Divider />
-
-      {/* SECOND ROW */}
-      <Box p="1rem 0">
-        <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
-          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
-          <Typography color={medium}>{location.name}</Typography>
-        </Box>
-        <Box display="flex" alignItems="center" gap="1rem">
-          <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
-          <Typography color={medium}>da cambiare</Typography>
-        </Box>
-      </Box>
-
-      <Divider />
-
-      {/* THIRD ROW */}
-      <Box p="1rem 0">
-        <FlexBetween mb="0.5rem">
-          <Typography color={medium}>Who's viewed your profile</Typography>
-          <Typography color={main} fontWeight="500">
-            {viewedProfile}
-          </Typography>
-        </FlexBetween>
-        <FlexBetween>
-          <Typography color={medium}>Impressions of your post</Typography>
-          <Typography color={main} fontWeight="500">
-            {impressions}
-          </Typography>
-        </FlexBetween>
-      </Box>
-
-      <Divider />
-
-      {/* FOURTH ROW */}
-      <Box p="1rem 0">
-        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
-          Social Profiles
-        </Typography>
-
-        <FlexBetween gap="1rem" mb="0.5rem">
-          <FlexBetween gap="1rem">
-            <img src="../assets/twitter.png" alt="twitter" />
-            <Box>
-              <Typography color={main} fontWeight="500">
-                Twitter
-              </Typography>
-              <Typography color={medium}>Social Network</Typography>
-            </Box>
-          </FlexBetween>
-          <EditOutlined sx={{ color: main }} />
-        </FlexBetween>
-
+      <FlexBetween gap="1rem" mb="0.5rem">
         <FlexBetween gap="1rem">
-          <FlexBetween gap="1rem">
-            <img src="../assets/linkedin.png" alt="linkedin" />
-            <Box>
-              <Typography color={main} fontWeight="500">
-                Linkedin
-              </Typography>
-              <Typography color={medium}>Network Platform</Typography>
-            </Box>
-          </FlexBetween>
-          <EditOutlined sx={{ color: main }} />
+          <Facebook fontSize="large" />
+          <Box>
+            <Typography color={main} fontWeight="500">
+              Facebook.user
+            </Typography>
+          </Box>
         </FlexBetween>
-      </Box>
+      </FlexBetween>
     </WidgetWrapper>
   );
 };
